f5069bd234a2d1e49337c89ebfef521f8a3aa874
vuduynhiennn <vuduynhiencute@gmail.com>
2021-12-05 22:10:03 +0700

    style index

--- project/index.php
+++ project/index.php
@@ -5,8 +5,39 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nhóm 25: web bán hàng</title>
+    <!-- import css  -->
+    <link rel="stylesheet" href="./style/reset.css">
+    <link rel="stylesheet" href="./style/main.css">
 </head>
 <body>
-    Khu vực trang chính
+    <header>
+
+    </header>
+
+    <div class="main">
+
+    </div>
+    
+    <footer>
+        <div class="footer--ctn">
+            <p class="bold">Đồ Án Tốt Nghiệp J2teamnnl, Trực thuộc J2team School</p>
+            <br>
+            <p>Ngày khai giảng: 05/09/2021, ngày bắt đầu làm đồ án tốt nghiệp 12/05/2021</p>
+            <br>
+            <p>Nhóm trưởng: Vũ Duy Nhiên, thành viên nhóm: Việt Nguyễn</p>
+            <br>
+            <p>Công nghệ sử dụng: html 5, css 3, js thuần, php thuần, mysql</p>
+            <br>
+            <p>github: <a href="https://github.com/vuduynhiennn/datn-j2teamnnl">datn-j2teamnnl</a></p>
+            <br>
+            <a href="#">
+                <img class="bocongthuong" src="https://media3.scdn.vn/img4/2020/12_16/XhpGDnvWqrlKeHLst3aS.png" alt="logo bộ công thương">
+            </a>
+            <a href="#">
+                <img class="bocongthuong" src="https://media3.scdn.vn/img4/2020/12_16/h6lEMGIAt4Uapd0Mls34.png" alt="logo bộ công thương">
+            </a>
+        </div>
+
+    </footer>
 </body>
 </html>
