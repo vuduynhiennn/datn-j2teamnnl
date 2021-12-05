ca29f53e9c81fecb2f79eb7e9b1e3e987dd48fea
vuduynhiennn <vuduynhiencute@gmail.com>
2021-12-05 21:19:45 +0700

    admin area

--- project/admin/index.php
+++ project/admin/index.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Nhóm 25: web bán hàng</title>
+    <title>Admin</title>
 </head>
 <body>
-    Khu vực trang chính
+    Khu vực admin
 </body>
 </html>
